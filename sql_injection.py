d10a2a029093e90189ee968525d73dca6b3ce0c3
Ryan Lyman <ryanlyman@gmail.com>
2026-05-29 08:48:14 -0600

    Query and test query validattion

--- sql_injection.py
+++ sql_injection.py
@@ -11,8 +11,22 @@ def create_query(username, password):
 
 # All team members need to create test cases for the function (making sure it's 
 # valid input) should have at least 3 test cases
+test_cases = [
+    ("ryan_00001",   "pass_word1"), 
+    ("Emily_m",  "Secure_99"),    
+    ("raquel_22",   "myPass_2007"),    
+]
+
 def test_create_query_validation(test_cases):
-    ...
+    print("=" * 60)
+    print("VALID INPUT TEST CASES")
+    print("=" * 60)
+    for username, password in test_cases:
+        query = create_query(username, password)
+        print(f"  user='{username}', pass='{password}'")
+        print(f"  → {query}")
+        print()
+
 
 #Testing 3 Tautology Attacks, 3 union query attacks, 3 additional statement attacks, and 3 comment attacks.
 # Each member should come up with at least one of each type of attack
@@ -44,6 +58,7 @@ def create_strong_sanitized_query(username, password):
 
 # This is main function that will run everything
 def main():
+    test_create_query_validation(test_cases)    
     ...
 
 if __name__ == "__main__":
